aea8a665e94f8a1ac4cc1d495bdcdef0aecdb798
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-12-29 17:50:27 +0200

    Fixed one migration that had the old hash column name

--- db/migrate/20181228215543_add_name_for_debug_symbol.rb
+++ db/migrate/20181228215543_add_name_for_debug_symbol.rb
@@ -1,6 +1,6 @@
 class AddNameForDebugSymbol < ActiveRecord::Migration[5.2]
   def change
     add_column :debug_symbols, :name, :string
-    add_index :debug_symbols, [:name, :hash], unique: true
+    add_index :debug_symbols, [:name, :symbol_hash], unique: true
   end
 end
