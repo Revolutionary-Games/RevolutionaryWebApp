43bb38904813296af51f8053961f465575833636
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-12-31 17:13:33 +0200

    Tweaked policies again, still doesn't work any better

--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -1,15 +1,15 @@
 class AdminUserPolicy
   # All AdminUsers share the same connection so we setup a class wide
   # connection available to any users who are admins.
-  regulate_class_connection { acting_user.admin? }
+  regulate_class_connection { admin? }
 
   # Admins can manage users
-  allow_change { acting_user.admin? }
+  # allow_change { acting_user.admin? }
   
   # All models are sent? except these some keys
   # password, password_confirmation and password_digest are never sent
   # also the token is not sent so admins can't impersonate others
-  regulate_all_broadcasts { |policy|
+  regulate_all_broadcasts {|policy|
     policy.send_all_but(:password, :password_confirmation, :password_digest, :api_token).
       to(AdminUserPolicy)
   }
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -3,7 +3,9 @@ class UserPolicy
   regulate_instance_connections { self }
 
   # And they get their own stuff
-  regulate_broadcast { |policy|
-    policy.send_all_but(:password, :password_confirmation, :password_digest).to(self)
+  regulate_broadcast {|policy|
+    # This doesn't work? Or maybe it does and the problem is the models not allowing access
+    # policy.send_all_but(:password, :password_confirmation, :password_digest).to(self)
+    policy.send_all_but(:password, :password_confirmation, :password_digest).to(policy.obj)
   }
 end
