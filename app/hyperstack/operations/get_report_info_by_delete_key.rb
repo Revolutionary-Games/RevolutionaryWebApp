c7d7c0412f36ba84d837b8657437edf0f3c61600
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-06-15 21:50:30 +0300

    Get report by key OP now works

--- app/hyperstack/operations/get_report_info_by_delete_key.rb
+++ app/hyperstack/operations/get_report_info_by_delete_key.rb
@@ -1,31 +1,12 @@
-# class GetReportInfoByDeleteKey < Hyperstack::ServerOp
-#   param :key
-#   param :acting_user, allow_nil: true
-#   add_error :acting_user, :invalid, "Invalid error" do
-#     false
-#   end  
-#   step {
-#     return "going well"
-#     report = Report.find_by delete_key: key
-#     if !report
-#       fail
-#     end
-
-#     [report.id, report.created_at, report.description]
-#   }
-# end
-
-
-class GetReportInfoByDeleteKey < Hyperstack::ControllerOp
+class GetReportInfoByDeleteKey < Hyperstack::ServerOp
+  param acting_user: nil, nils: true
   param :key
-  step {
-    return "success"
-    report = Report.find_by delete_key: key
-    if !report
-      fail
-    end
+  add_error :key, :no_valid_key, "No valid key provided" do
+    !Report.find_by_delete_key(params.key)
+  end
 
+  step {
+    report = Report.find_by delete_key: params.key
     [report.id, report.created_at, report.description]
   }
 end
-
