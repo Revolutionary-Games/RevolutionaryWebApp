2c6758ece389cbfa9a15bfb9bf0ce96e8bf47a55
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-06-18 20:21:33 +0300

    Changed the name of the discord webhook return URL variable

--- app/helpers/discord_webhook.rb
+++ app/helpers/discord_webhook.rb
@@ -7,7 +7,7 @@ module DiscordWebhook
   # TODO: would be nice to run this with sidekiq or something
   def self.sendCreatedReport(report)
     logger = Rails.logger
-    if !ENV["DISCORD_REPORT_WEBHOOK"] || !ENV["DISCORD_RETURN_URL"]
+    if !ENV["DISCORD_REPORT_WEBHOOK"] || !ENV["BASE_URL"]
       logger.debug "Discord webhook disabled due to missing env variables"
       return
     end
@@ -22,7 +22,7 @@ module DiscordWebhook
     # This is a hacky way to follow the rate limiting
     builder = Discordrb::Webhooks::Builder.new
 
-    reportURL = URI::join(ENV["DISCORD_RETURN_URL"], "/report/#{report.id}").to_s
+    reportURL = URI::join(ENV["BASE_URL"], "/report/#{report.id}").to_s
     
     builder.content = "New report created: #{reportURL}"
 
