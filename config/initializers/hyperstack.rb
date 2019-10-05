205c8ec01abbd00fe0a2b5431cc71b9cdd87d070
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-10-05 20:59:31 +0300

    Enabled prerendering

--- config/initializers/hyperstack.rb
+++ config/initializers/hyperstack.rb
@@ -2,7 +2,7 @@
 # If you are not using ActionCable, see http://hyperstack.orgs/docs/models/configuring-transport/
 Hyperstack.configuration do |config|
   config.transport = :action_cable
-  config.prerendering = :off # or :on
+  config.prerendering = :on # or :on
   config.cancel_import 'react/react-source-browser' # bring your own React and ReactRouter via Yarn/Webpacker
   config.import 'hyperstack/component/jquery', client_only: true # remove this line if you don't need jquery
   config.import 'hyperstack/hotloader', client_only: true if Rails.env.development?
