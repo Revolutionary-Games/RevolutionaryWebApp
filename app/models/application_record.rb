ad5ac412e1ac7d651cf52028b90062ccf9d74c30
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-10-20 14:25:32 +0300

    Hyperstack production recommended fixes
    let's hope these work

--- app/models/application_record.rb
+++
@@ -1,5 +0,0 @@
-# app/models/application_record.rb
-# the presence of this file prevents rails migrations from recreating application_record.rb
-# see https://github.com/rails/rails/issues/29407
-
-require "#{Rails.root}/app/hyperstack/models/application_record.rb"
