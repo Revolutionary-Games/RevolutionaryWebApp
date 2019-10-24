6b86779e009d7a8cb00a1a41dad782ce0b65b1f6
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-10-24 19:48:35 +0300

    Fixed trying to view current user page when not logged in

--- app/hyperstack/components/current_user.rb
+++ app/hyperstack/components/current_user.rb
@@ -1,12 +1,19 @@
+# frozen_string_literal: true
+
 class CurrentUser < HyperComponent
   # To quiet some react warnings
   include Hyperstack::Router::Helpers
 
   render(DIV) do
+    unless App.acting_user
+      P { 'No current user' }
+      return
+    end
+
     H1 { App.acting_user.email }
 
     P {
-      "Here are your details:"
+      'Here are your details:'
     }
 
     UserProperties(user: App.acting_user)
