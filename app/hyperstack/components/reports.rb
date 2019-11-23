fb408c3ebbe008b179e07bdb9ae819864d06265c
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-11-24 00:42:58 +0200

    Added pagination to reports and changed the shown data a bit

--- app/hyperstack/components/reports.rb
+++ app/hyperstack/components/reports.rb
@@ -1,46 +1,60 @@
+# frozen_string_literal: true
+
+# Single table line
 class ReportItem < HyperComponent
   include Hyperstack::Router::Helpers
-  
+
   param :report
   render(TR) do
-    TD{"#{@Report.game_version}"}
-    # TD{"#{@Report.id}"}
-    TD{Link("/report/#{@Report.id}") { "#{@Report.updated_at}" }}
-    TD{@Report.solved ? "yes" : "no"}
-    TD{"#{@Report.crash_time}"}
-    TD{"#{@Report.description}"}
-    TD{"#{@Report.public}"}
-    TD{"#{@Report.solved_comment}"}
+    TD { @Report.game_version.to_s }
+    TH(scope: 'row') { @Report.id.to_s }
+    TD { Link("/report/#{@Report.id}") { @Report.updated_at.to_s } }
+    TD { @Report.solved ? 'yes' : 'no' }
+    # TD{"#{@Report.crash_time}"}
+    TD { @Report.description.to_s }
+    TD { @Report.public.to_s }
+    TD { @Report.solved_comment.to_s }
   end
 end
 
-class Reports < HyperComponent  
+# A table of crash reports
+class Reports < HyperComponent
+  param :current_page, default: 0, type: Integer
+  param :page_size, default: 25, type: Integer
+
+  def items
+    Report.visible_to(Hyperstack::Application.acting_user_id).index_by_updated_at
+  end
+
   render(DIV) do
+    H1 { 'Crash reports' }
 
-    H1 { "Crash reports" }
+    Paginator(current_page: @CurrentPage,
+              page_size: @PageSize,
+              item_count: items.count,
+              on_page_changed: lambda { |page|
+                mutate @CurrentPage = page
+              })
 
-    TABLE {
-
+    ReactStrap.Table(:striped, :responsive) {
       THEAD {
-        TR{
-          TD{ "Version" }
-          # TD{ "ID" }
-          TD{ "Updated At" }
-          TD{ "Solved" }
-          TD{ "Crash Time" }
-          TD{ "Description" }
-          TD{ "Public" }
-          TD{ "Solve comment" }
+        TR {
+          TH { 'Version' }
+          TH { 'ID' }
+          TH { 'Updated At' }
+          TH { 'Solved' }
+          # TH{ "Crash Time" }
+          TH { 'Description' }
+          TH { 'Public' }
+          TH { 'Solve comment' }
         }
       }
 
-      TBODY{
-        Report.visible_to(Hyperstack::Application.acting_user_id).index_by_updated_at.each{
-          |report|
+      TBODY {
+        items.offset(@CurrentPage * @PageSize).take(@PageSize).each { |report|
           ReportItem(report: report)
         }
       }
-    }    
-
+    }
   end
 end
