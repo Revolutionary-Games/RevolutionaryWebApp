b8ba1feaaca1fb0318a81cafde775763c438c2cb
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-11-24 00:31:55 +0200

    Updated the home page text

--- app/hyperstack/components/home.rb
+++ app/hyperstack/components/home.rb
@@ -1,28 +1,28 @@
+# frozen_string_literal: true
+
+# Main page content component
 class Home < HyperComponent
   include Hyperstack::Router
 
-  # after_mount do
-  #   CurrentUserDetails.run.then do |value|
-  #     mutate @user = value
-  #   end.fail do |error|
-  #     mutate @user = "Error: #{error}"
-  #   end    
-  # end
-  
   render(DIV) do
-
-    H1 { "This is an inprogress Thrive developer hub." }
-
-    H3 { "The Thrive Launcher is now able to create crash reports." }
-
-    P { "If you want to decode a crash dump you can use the link below to go " + 
-        "to the plain crash dump processing tool." }
-
-    H3 { Link('/crashdump-tool') { 'Process a crash here' } }
+    H1 { 'Welcome to Thrive developer center' }
 
     P {
-      "If you are a Thrive developer log in using your Dev Forum account to see more options"
+      SPAN {
+        'This is a place where a bunch of different tools and services for use in ' \
+             'Thrive developed are collected. If you want to report a crash please use the ' \
+             'reporter in the thrive launcher. If you want to report some other kind of ' \
+             'issue, please head on over to our '
+      }
+      A(href: 'https://community.revolutionarygamesstudio.com/') { 'community forums.' }
     }
-    
+
+    P {
+      'If you are a Thrive developer log in using your Dev Forum account to see more options.'
+    }
+
+    P {
+      'The manual crash dump file decoding option is now under "Tools"'
+    }
   end
 end
