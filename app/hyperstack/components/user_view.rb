41bf64f2ed70295fdc3f99bcbd5eee84dc16ef8d
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-11-24 12:57:21 +0200

    Updated user page to user reactstrap

--- app/hyperstack/components/user_view.rb
+++ app/hyperstack/components/user_view.rb
@@ -1,127 +1,123 @@
+# frozen_string_literal: true
+
+# View of single user's properties
 class UserProperties < HyperComponent
   param :user, type: User
   param :showToken, default: false, type: Boolean
   param :showLFS, default: false, type: Boolean
 
   render(DIV) do
-
     lookingAtSelf = @User == App.acting_user
 
-    if lookingAtSelf
-      P { "This is you" }
-    end
+    P { 'This is you' } if lookingAtSelf
 
-    P { "email: #{ @User.email}" }
-    P { "name: #{ @User.name}" }
-    P { "admin: #{ @User.admin}" }
-    P { "developer: #{ @User.developer}" }
-    P { "sso_source: #{ @User.sso_source}" }
-    P { "local: #{ @User.local}" }
+    P { "email: #{@User.email}" }
+    P { "name: #{@User.name}" }
+    P { "admin: #{@User.admin}" }
+    P { "developer: #{@User.developer}" }
+    P { "sso_source: #{@User.sso_source}" }
+    P { "local: #{@User.local}" }
 
     hasToken = @User.has_api_token
     hasLFS = @User.has_lfs_token
 
     P { "api token: #{hasToken}" }
 
-    if hasToken == "true" && lookingAtSelf
-      BUTTON { if !@ShowToken then 'View Token' else 'Hide Token' end }.on(:click){
+    if hasToken == 'true' && lookingAtSelf
+      RS.Button(color: 'primary') { !@ShowToken ? 'View Token' : 'Hide Token' }.on(:click) {
         mutate @ShowToken = !@ShowToken
       }
 
-      if @ShowToken
-        P { "Your token is: #{@User.api_token}" }
-      end
+      P { "Your token is: #{@User.api_token}" } if @ShowToken
 
-      BUTTON { "Regenerate Token"}.on(:click){
+      RS.Button(color: 'secondary') { 'Regenerate Token' }.on(:click) {
         GenerateAPITokenForUser.run
-          .then { mutate @ShowToken = true }
-          .fail { alert "failed to run operation" }
+                               .then { mutate @ShowToken = true }
+                               .fail { alert 'failed to run operation' }
       }
 
-      BUTTON { "Clear Token" }.on(:click){
+      RS.Button(color: 'secondary') { 'Clear Token' }.on(:click) {
         ResetAPITokenForUser.run(user_id: @User.id).then {
           @User.has_api_token!
           mutate @ShowToken = false
-        }.fail { alert "failed to run operation" }
+        }.fail { alert 'failed to run operation' }
       }
 
     elsif lookingAtSelf
 
-      BUTTON { "Generate API Token"}.on(:click){
+      RS.Button(color: 'primary') { 'Generate API Token' }.on(:click) {
         GenerateAPITokenForUser.run.then {
           @User.has_api_token!
           mutate @ShowToken = true
-        }.fail { alert "failed to run operation" }
+        }.fail { alert 'failed to run operation' }
       }
     end
 
     P { "Git lfs token: #{hasLFS}" }
 
-    if hasLFS == "true" && lookingAtSelf
-      BUTTON { if !@ShowLFS then 'View LFS Token' else 'Hide LFS Token' end }.on(:click){
+    if hasLFS == 'true' && lookingAtSelf
+      RS.Button(color: 'primary') {
+        !@ShowLFS ? 'View LFS Token' : 'Hide LFS Token'
+      }.on(:click) {
         mutate @ShowLFS = !@ShowLFS
       }
 
-      if @ShowLFS
-        P { "Your Git LFS token is: #{@User.lfs_token}" }
-      end
+      P { "Your Git LFS token is: #{@User.lfs_token}" } if @ShowLFS
 
-      BUTTON { "Regenerate Token"}.on(:click){
+      RS.Button(color: 'secondary') { 'Regenerate Token' }.on(:click) {
         GenerateLFSTokenForUser.run
-          .then { mutate @ShowLFS = true }
-          .fail { alert "failed to run operation" }
+                               .then { mutate @ShowLFS = true }
+                               .fail { alert 'failed to run operation' }
       }
 
-      BUTTON { "Clear Token" }.on(:click){
+      RS.Button(color: 'secondary') { 'Clear Token' }.on(:click) {
         ResetLFSTokenForUser.run(user_id: @User.id).then {
           @User.has_lfs_token!
           mutate @ShowLFS = false
-        }.fail { alert "failed to run operation" }
+        }.fail { alert 'failed to run operation' }
       }
 
     elsif lookingAtSelf
 
-      BUTTON { "Generate Git LFS Token"}.on(:click){
-
+      RS.Button(color: 'primary') { 'Generate Git LFS Token' }.on(:click) {
         GenerateLFSTokenForUser.run.then {
           @User.has_lfs_token!
           mutate @ShowLFS = true
-        }.fail { alert "failed to run operation" }
+        }.fail { alert 'failed to run operation' }
       }
     end
 
-    if !lookingAtSelf
-      H2 {"Actions"}
-      BUTTON { 'Force Clear Tokens' }.on(:click){
+    unless lookingAtSelf
+      H2 { 'Actions' }
+      RS.Button(color: 'danger') { 'Force Clear Tokens' }.on(:click) {
         ResetLFSTokenForUser.run(user_id: @User.id).then {
           ResetAPITokenForUser.run(user_id: @User.id)
-        }.then{
+        }.then {
           @User.has_lfs_token!
           @User.has_api_token!
-          alert "success"
-        }.fail{ alert "failed to run operation" }
+          alert 'success'
+        }.fail { alert 'failed to run operation' }
       }
     end
   end
 end
 
+# View of a single user
 class UserView < HyperComponent
   include Hyperstack::Router::Helpers
 
   render(DIV) do
-
     user = User.find_by_id match.params[:id]
 
-    if !user
+    unless user
       H1 { "No user exists with id = #{match.params[:id]}" }
       return
     end
 
-    H1 { "User #{user.email} (#{user.id})" }
+    H1 { "User #{user.email} (id: #{user.id})" }
 
-    H2 { "Properties" }
+    H2 { 'Properties' }
 
     UserProperties(user: user)
-
   end
 end
