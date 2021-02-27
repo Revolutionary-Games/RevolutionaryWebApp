307bd31e3bdd3336c49e2b122918ff3d73f4c8fb
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-02-27 14:54:57 +0200

    Updated the blazor template

--- Server/Controllers/WeatherForecastController.cs
+++ Server/Controllers/WeatherForecastController.cs
@@ -1,10 +1,10 @@
-﻿using ThriveDevCenter.Shared;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ThriveDevCenter.Shared;
 
 namespace ThriveDevCenter.Server.Controllers
 {
@@ -17,11 +17,11 @@ namespace ThriveDevCenter.Server.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            this.logger = logger;
+            _logger = logger;
         }
 
         [HttpGet]
