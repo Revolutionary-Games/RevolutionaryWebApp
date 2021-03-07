199f49febbd78b0b6940911001dac5dbbcaa9f93
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-03-07 19:08:09 +0200

    LFS item sizes are now back to being printed as MiB

---
+++ Shared/ValuePrintHelpers.cs
@@ -0,0 +1,26 @@
+namespace ThriveDevCenter.Shared
+{
+    using System;
+    using System.Globalization;
+
+    public static class ValuePrintHelpers
+    {
+        public const float KIBIBYTE = 1024;
+        public const float MEBIBYTE = KIBIBYTE * KIBIBYTE;
+
+        public static string BytesToMiB(this float number, int decimals = 2, bool suffix = true)
+        {
+            var result = Math.Round((number / MEBIBYTE), decimals).ToString(CultureInfo.CurrentCulture);
+
+            if (!suffix)
+                return result;
+
+            return result + " MiB";
+        }
+
+        public static string BytesToMiB(this long number, int decimals = 2, bool suffix = true)
+        {
+            return ((float)number).BytesToMiB(decimals, suffix);
+        }
+    }
+}
