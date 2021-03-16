d18c6fd013fabf1f737c7c8cec5e4d24417f6099
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-03-16 19:55:27 +0200

    Registration now works

--- Server/Controllers/RegistrationController.cs
+++ Server/Controllers/RegistrationController.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ThriveDevCenter.Server.Controllers
 {
+    using System.Linq;
+    using System.Threading.Tasks;
     using Authorization;
     using Hubs;
     using Microsoft.AspNetCore.SignalR;
     using Microsoft.Extensions.Logging;
     using Models;
     using Shared;
+    using Shared.Models;
 
     [ApiController]
     [Route("api/v1/[controller]")]
@@ -40,7 +43,7 @@ namespace ThriveDevCenter.Server.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post(RegistrationFormData request)
+        public async Task<IActionResult> Post(RegistrationFormData request)
         {
             if (!csrfVerifier.IsValidCSRFToken(request.CSRF))
                 return BadRequest("Invalid CSRF");
@@ -48,18 +51,34 @@ namespace ThriveDevCenter.Server.Controllers
             if (!SecurityHelpers.SlowEquals(request.RegistrationCode, configuration.RegistrationCode))
                 return BadRequest("Invalid registration code");
 
-            if(request.Name == null || request.Name.Length < 3)
+            if (request.Name == null || request.Name.Length < 3)
                 return BadRequest("Name is too short");
 
-            if(request.Email == null || request.Email.Length < 3 || !request.Email.Contains('@'))
+            if (request.Email == null || request.Email.Length < 3 || !request.Email.Contains('@'))
                 return BadRequest("Email is invalid");
 
-            if(request.Password == null || request.Password.Length < 6)
+            if (request.Password == null || request.Password.Length < 6)
                 return BadRequest("Password is too short");
+
+            // Check for conflicting username or email
+            if (database.Users.FirstOrDefault(u => u.UserName == request.Name) != null ||
+                database.Users.FirstOrDefault(u => u.Email == request.Email) != null)
+                return BadRequest("There is already an account associated with the given email or name");
 
             var password = Passwords.CreateSaltedPasswordHash(request.Password);
 
-            return BadRequest("Not implemented");
+            var user = new User()
+            {
+                Email = request.Email,
+                UserName = request.Name,
+                PasswordHash = password,
+                Local = true
+            };
+
+            await database.Users.AddAsync(user);
+            await database.SaveChangesAsync();
+
+            return Created($"/users/{user.Id}", user.GetInfo(RecordAccessLevel.Private));
         }
     }
 }
