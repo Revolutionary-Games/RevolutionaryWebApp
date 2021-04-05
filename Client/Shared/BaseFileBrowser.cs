31244a47c3c047fe920b7e4275c4329571cd6103
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-04-05 18:55:15 +0300

    Created a base class for file browsers

---
+++ Client/Shared/BaseFileBrowser.cs
@@ -0,0 +1,54 @@
+namespace ThriveDevCenter.Client.Shared
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components;
+    using ThriveDevCenter.Shared;
+    using ThriveDevCenter.Shared.Models;
+
+    public abstract class BaseFileBrowser<T> : PaginatedPage<T>
+        where T : class, IIdentifiable
+    {
+        [Parameter]
+        public string FileBrowserPath { get; set; }
+
+        [Parameter]
+        public string BasePath { get; set; }
+
+        [Parameter]
+        public string RootFolderName { get; set; }
+
+        protected BaseFileBrowser() : base(new SortHelper("Name", SortDirection.Ascending))
+        {
+            DefaultPageSize = 100;
+        }
+
+        /// <summary>
+        ///   When true reacts to parameters changing by querying the server again
+        /// </summary>
+        public bool ReactToParameterChange { get; protected set; }
+
+        protected string CurrentPathSlashPrefix => "/" + (FileBrowserPath ?? string.Empty);
+
+        protected string SlashIfPathNotEmpty => string.IsNullOrEmpty(FileBrowserPath) ? string.Empty : "/";
+
+        protected string FolderLink(string name)
+        {
+            return $"{BasePath}{FileBrowserPath}{SlashIfPathNotEmpty}{name}";
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await base.OnParametersSetAsync();
+
+            if (ReactToParameterChange)
+                await FetchData();
+        }
+
+        protected override Task OnQuerySent(Dictionary<string, string> requestParams)
+        {
+            ReactToParameterChange = true;
+            return base.OnQuerySent(requestParams);
+        }
+    }
+}
