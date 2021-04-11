e196373a04b7e09d28f48174103ff4e9ce71ad3c
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-04-11 16:56:22 +0300

    Added GUI and API to create LFS projects

--- Server/Controllers/LFSProjectController.cs
+++ Server/Controllers/LFSProjectController.cs
@@ -7,10 +7,12 @@ namespace ThriveDevCenter.Server.Controllers
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
     using Authorization;
     using BlazorPagination;
     using Filters;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Models;
     using Shared;
@@ -113,6 +115,63 @@ namespace ThriveDevCenter.Server.Controllers
                 .OrderByDescending(l => l.Id).ToPagedResultAsync(page, pageSize);
 
             return objects.ConvertResult(i => i.GetDTO());
+        }
+
+        [AuthorizeRoleFilter(RequiredAccess = UserAccessLevel.Admin)]
+        [HttpPost]
+        public async Task<ActionResult> CreateNew([Required] LFSProjectDTO projectInfo)
+        {
+            var project = new LfsProject()
+            {
+                Name = projectInfo.Name,
+                Slug = projectInfo.Slug,
+                Public = projectInfo.Public,
+                RepoUrl = projectInfo.RepoUrl,
+                CloneUrl = projectInfo.CloneUrl
+            };
+
+            var results = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(project, new ValidationContext(project), results, true))
+            {
+                return BadRequest("Invalid properties given for a new project, errors: " +
+                    string.Join(", ", results.Select(r => r.ToString())));
+            }
+
+            if (!Regex.IsMatch(project.Slug, @"^[\w-]{2,15}$"))
+                return BadRequest("Invalid slug, either too long, short, or uses disallowed characters");
+
+            if (project.Name.Length < 3 || project.Name.Length > 100)
+                return BadRequest("Project name is too long or too short");
+
+            // Check for duplicate data
+            if (await database.LfsProjects.AsQueryable().Where(p => p.Name == project.Name).AnyAsync() ||
+                await database.LfsProjects.AsQueryable().Where(p => p.Slug == project.Slug).AnyAsync())
+            {
+                return BadRequest("Project name or slug is already in-use");
+            }
+
+            // TODO: could maybe save the project first in order to get the ID for the log message...
+            var action = new AdminAction()
+            {
+                Message = $"New LFS project created, slug: {project.Slug}, name: {project.Name}",
+                PerformedById = HttpContext.AuthenticatedUser().Id
+            };
+
+            await database.LfsProjects.AddAsync(project);
+            await database.AdminActions.AddAsync(action);
+
+            try
+            {
+                await database.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                logger.LogWarning("Creating new LFSProject failed due to db error: {@E}", e);
+                return BadRequest(
+                    "Error saving data to database, if the error persists the data likely violates some constraints");
+            }
+
+            return Created($"/lfs/{project.Id}", project.GetDTO(ComputeProjectGitLFSAccessUrl(project)));
         }
 
         [NonAction]
