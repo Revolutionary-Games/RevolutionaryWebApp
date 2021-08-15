358c8d0540977897cd2f207a5397d9689d6298f7
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-08-15 16:52:51 +0300

    Refactored common provisioning parts to a base class

--- Server/Jobs/ProvisionControlledServerJob.cs
+++ Server/Jobs/ProvisionControlledServerJob.cs
@@ -2,38 +2,27 @@ namespace ThriveDevCenter.Server.Jobs
 {
     using System;
     using System.Collections.Generic;
-    using System.Net.Sockets;
     using System.Threading;
     using System.Threading.Tasks;
     using Hangfire;
     using Microsoft.Extensions.Logging;
     using Models;
-    using Renci.SshNet.Common;
     using Services;
     using Shared.Models;
 
-    public class ProvisionControlledServerJob
+    public class ProvisionControlledServerJob : BaseProvisionServerJob
     {
-        // TODO: put this somewhere more sensible
-        public const string GeneralProvisionCommandPart =
-            "sudo dnf install -y podman curl git git-lfs dotnet-runtime-5.0 && " +
-            "git lfs install && sudo mkdir -p /executor_cache";
-
         private const string ProvisioningCommand = GeneralProvisionCommandPart +
             " && sudo chown -R centos:centos /executor_cache";
-
-        private readonly ILogger<ProvisionControlledServerJob> logger;
-        private readonly NotificationsEnabledDb database;
+        
         private readonly IEC2Controller ec2Controller;
         private readonly IBackgroundJobClient jobClient;
         private readonly ControlledServerSSHAccess sshAccess;
 
         public ProvisionControlledServerJob(ILogger<ProvisionControlledServerJob> logger,
             NotificationsEnabledDb database, IEC2Controller ec2Controller, IBackgroundJobClient jobClient,
-            ControlledServerSSHAccess sshAccess)
+            ControlledServerSSHAccess sshAccess) : base(logger, database)
         {
-            this.logger = logger;
-            this.database = database;
             this.ec2Controller = ec2Controller;
             this.jobClient = jobClient;
             this.sshAccess = sshAccess;
@@ -41,19 +30,10 @@ namespace ThriveDevCenter.Server.Jobs
 
         public async Task Execute(long id, CancellationToken cancellationToken)
         {
-            var server = await database.ControlledServers.FindAsync(id);
+            var server = await Database.ControlledServers.FindAsync(id);
 
-            if (server == null)
-            {
-                logger.LogWarning("Server {Id} not found for provisioning", id);
+            if (CheckServerDataIsFineForProvisioning(id, server))
                 return;
-            }
-
-            if (server.ProvisionedFully)
-                return;
-
-            if (server.Status != ServerStatus.Provisioning)
-                throw new Exception("Server status is not Provisioning for the provisioning job");
 
             // Check that the server is running before starting provisioning
             foreach (var status in await ec2Controller.GetInstanceStatuses(new List<string>() { server.InstanceId },
@@ -69,73 +49,33 @@ namespace ThriveDevCenter.Server.Jobs
                     // We can now perform the provisioning
                     server.PublicAddress = EC2Controller.InstanceIP(status);
                     server.RunningSince = DateTime.UtcNow;
-                    await PerformProvisioningCommands(server);
-                    break;
+                    await PerformProvisioningCommands(server, ProvisioningCommand);
                 }
                 else
                 {
-                    logger.LogInformation("Waiting for server {Id} (instance: {InstanceId}) to boot up", id,
+                    Logger.LogInformation("Waiting for server {Id} (instance: {InstanceId}) to boot up", id,
                         server.InstanceId);
                     server.StatusLastChecked = DateTime.UtcNow;
                     server.BumpUpdatedAt();
-                    await database.SaveChangesAsync(cancellationToken);
+                    await Database.SaveChangesAsync(cancellationToken);
                 }
+                
+                break;
             }
 
             // If not provisioned yet, need to requeue this job
             if (!server.ProvisionedFully)
             {
-                logger.LogTrace("Server {Id} not yet fully provisioned", id);
+                Logger.LogTrace("Server {Id} not yet fully provisioned", id);
                 jobClient.Schedule<ProvisionControlledServerJob>(x => Execute(id, CancellationToken.None),
                     TimeSpan.FromSeconds(10));
             }
         }
 
-        // TODO: make this async
-        private async Task PerformProvisioningCommands(ControlledServer server)
+        protected override BaseSSHAccess ConnectWithSSH(BaseServer server)
         {
-            logger.LogInformation("Beginning SSH connect to provision server at: {PublicAddress}",
-                server.PublicAddress);
-
-            // TODO: there should probably be a maximum number of times this is attempted
-            try
-            {
-                sshAccess.ConnectTo(server.PublicAddress.ToString());
-            }
-            catch (SocketException)
-            {
-                logger.LogWarning("Connection failed (socket exception), server is probably not up yet");
-                return;
-            }
-            catch (SshOperationTimeoutException)
-            {
-                logger.LogWarning("Connection failed (ssh timed out), server is probably not up yet");
-                return;
-            }
-
-            logger.LogInformation("Connected, running provisioning command...");
-
-            var start = DateTime.UtcNow;
-            var result = sshAccess.RunCommand(ProvisioningCommand);
-
-            if (!result.Success)
-            {
-                logger.LogWarning("Failed provision result ({ExitCode}: {Result}", result.ExitCode, result.Result);
-                throw new Exception($"Provisioning commands failed ({result.ExitCode}): {result.Error}");
-            }
-
-            // Now fully provisioned
-            server.ProvisionedFully = true;
-            server.Status = ServerStatus.Running;
-            server.LastMaintenance = DateTime.UtcNow;
-            server.WantsMaintenance = false;
-            server.StatusLastChecked = DateTime.UtcNow;
-            server.ReservationType = ServerReservationType.None;
-            server.BumpUpdatedAt();
-            await database.SaveChangesAsync();
-
-            var elapsed = DateTime.UtcNow - start;
-            logger.LogInformation("Completed provisioning for server {Id}, elapsed: {Elapsed}", server.Id, elapsed);
+            sshAccess.ConnectTo(server.PublicAddress.ToString());
+            return sshAccess;
         }
     }
 }
--- Server/Jobs/ProvisionExternalServerJob.cs
+++ Server/Jobs/ProvisionExternalServerJob.cs
@@ -1,107 +1,56 @@
 namespace ThriveDevCenter.Server.Jobs
 {
     using System;
-    using System.Net.Sockets;
     using System.Threading;
     using System.Threading.Tasks;
     using Hangfire;
     using Microsoft.Extensions.Logging;
     using Models;
-    using Renci.SshNet.Common;
     using Services;
     using Shared.Models;
 
-    public class ProvisionExternalServerJob
+    public class ProvisionExternalServerJob : BaseProvisionServerJob
     {
-        private readonly ILogger<ProvisionExternalServerJob> logger;
-        private readonly NotificationsEnabledDb database;
         private readonly IBackgroundJobClient jobClient;
         private readonly ExternalServerSSHAccess sshAccess;
 
         public ProvisionExternalServerJob(ILogger<ProvisionExternalServerJob> logger, NotificationsEnabledDb database,
-            IBackgroundJobClient jobClient,
-            ExternalServerSSHAccess sshAccess)
+            IBackgroundJobClient jobClient, ExternalServerSSHAccess sshAccess) : base(logger, database)
         {
-            this.logger = logger;
-            this.database = database;
             this.jobClient = jobClient;
             this.sshAccess = sshAccess;
         }
 
         public async Task Execute(long id, CancellationToken cancellationToken)
         {
-            var server = await database.ExternalServers.FindAsync(id);
+            var server = await Database.ExternalServers.FindAsync(id);
 
-            if (server == null)
-            {
-                logger.LogWarning("Server {Id} (external) not found for provisioning", id);
-                return;
-            }
-
-            if (server.ProvisionedFully)
+            if (CheckServerDataIsFineForProvisioning(id, server))
                 return;
 
-            if (server.Status != ServerStatus.Provisioning)
-                throw new Exception("Server status is not Provisioning for the provisioning job");
-
-            PerformProvisioningCommands(server);
+            await PerformProvisioningCommands(server, GeneralProvisionCommandPart);
 
             server.StatusLastChecked = DateTime.UtcNow;
             server.BumpUpdatedAt();
-            await database.SaveChangesAsync(cancellationToken);
+            await Database.SaveChangesAsync(cancellationToken);
 
             // If not provisioned yet, need to requeue this job
             if (!server.ProvisionedFully)
             {
-                logger.LogTrace("Server {Id} not yet fully provisioned", id);
+                Logger.LogTrace("External server {Id} not yet fully provisioned", id);
                 jobClient.Schedule<ProvisionControlledServerJob>(x => Execute(id, CancellationToken.None),
                     TimeSpan.FromSeconds(10));
             }
+            else
+            {
+                Logger.LogTrace("External server {Id} is provisioned now", id);
+            }
         }
 
-        // TODO: make this async
-        private void PerformProvisioningCommands(ExternalServer server)
+        protected override BaseSSHAccess ConnectWithSSH(BaseServer server)
         {
-            logger.LogInformation("Beginning SSH connect to provision external server at: {PublicAddress}",
-                server.PublicAddress);
-
-            // TODO: there should probably be a maximum number of times this is attempted
-            try
-            {
-                sshAccess.ConnectTo(server.PublicAddress.ToString(), server.SSHKeyFileName);
-            }
-            catch (SocketException)
-            {
-                logger.LogWarning("Connection failed (socket exception), server is probably not up yet");
-                return;
-            }
-            catch (SshOperationTimeoutException)
-            {
-                logger.LogWarning("Connection failed (ssh timed out), server is probably not up yet");
-                return;
-            }
-
-            logger.LogInformation("Connected, running provisioning command...");
-
-            var start = DateTime.UtcNow;
-            var result = sshAccess.RunCommand(ProvisionControlledServerJob.GeneralProvisionCommandPart);
-
-            if (!result.Success)
-            {
-                logger.LogWarning("Failed provision result ({ExitCode}: {Result}", result.ExitCode, result.Result);
-                throw new Exception($"Provisioning commands failed ({result.ExitCode}): {result.Error}");
-            }
-
-            // Now fully provisioned
-            server.ProvisionedFully = true;
-            server.Status = ServerStatus.Running;
-            server.LastMaintenance = DateTime.UtcNow;
-            server.WantsMaintenance = false;
-            server.ReservationType = ServerReservationType.None;
-
-            var elapsed = DateTime.UtcNow - start;
-            logger.LogInformation("Completed provisioning for external server {Id}, elapsed: {Elapsed}", server.Id,
-                elapsed);
+            sshAccess.ConnectTo(server.PublicAddress.ToString(), ((ExternalServer)server).SSHKeyFileName);
+            return sshAccess;
         }
     }
 }
