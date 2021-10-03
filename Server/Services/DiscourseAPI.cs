1c92dbc7520068d6c2337b772f55be44e04004f9
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-10-03 18:28:55 +0300

    Switch discourse user finding by email to not request the email back
    this should avoid log spam regarding reading this info from discourse

--- Server/Services/DiscourseAPI.cs
+++ Server/Services/DiscourseAPI.cs
@@ -69,6 +69,9 @@ namespace ThriveDevCenter.Server.Services
         /// <param name="includeNonActive">
         ///   If true even suspended users (ie. users that discourse doesn't consider "active") can be returned
         /// </param>
+        /// <param name="avoidEmailQuery">
+        ///   If true doesn't query emails back from discourse (avoids log spam). Will fail with multiple results.
+        /// </param>
         /// <returns>The user or null</returns>
         /// <remarks>
         ///   <para>
@@ -77,7 +80,7 @@ namespace ThriveDevCenter.Server.Services
         ///   </para>
         /// </remarks>
         public async Task<DiscourseUser> FindUserByEmail(string email, CancellationToken cancellationToken,
-            bool includeNonActive = false)
+            bool includeNonActive = false, bool avoidEmailQuery = true)
         {
             ThrowIfNotConfigured();
 
@@ -94,17 +97,38 @@ namespace ThriveDevCenter.Server.Services
                 new Dictionary<string, string>()
                 {
                     { "email", email },
-                    { "show_emails", "true" },
+                    { "show_emails", avoidEmailQuery ? "false" : "true" },
                 });
 
             return await PerformWithRateLimitRetries(
-                async () => (await httpClient.GetFromJsonAsync<List<DiscourseUser>>(url, cancellationToken) ??
-                    throw new Exception("Discourse didn't return a list")).FirstOrDefault(),
-                cancellationToken);
+                async () =>
+                {
+                    var list = await httpClient.GetFromJsonAsync<List<DiscourseUser>>(url, cancellationToken);
+                    if (list == null)
+                        throw new Exception("Discourse didn't return a list");
+
+                    if (avoidEmailQuery)
+                    {
+                        if (list.Count > 1)
+                        {
+                            throw new Exception(
+                                "Multiple users found with email, can't differentiate due to avoidEmailQuery");
+                        }
+
+                        if (list.Count > 0)
+                        {
+                            // Assume returned user matched exactly the email. This avoids requesting the email
+                            // data from discourse
+                            list[0].Email = email;
+                        }
+                    }
+
+                    return list.FirstOrDefault();
+                }, cancellationToken);
         }
 
         /// <summary>
-        ///   Gets discourse user info. This variant returns way more information that <see cref="FindUserByEmail"/>
+        ///   Gets discourse user info. This variant returns way more information than <see cref="FindUserByEmail"/>
         /// </summary>
         public async Task<DiscourseSingleUserInfo> UserInfoByName(string username, CancellationToken cancellationToken)
         {
