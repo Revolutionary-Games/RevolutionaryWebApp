33a401fe212ed120fe784e2a043cd6576f79192d
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-07-03 13:29:49 +0300

    Added some more meeting poll tests

--- Server.Tests/Models.Tests/MeetingPollTests.cs
+++ Server.Tests/Models.Tests/MeetingPollTests.cs
@@ -20,44 +20,44 @@ namespace ThriveDevCenter.Server.Tests.Models.Tests
                     {
                         { 1, new PollData.PollChoice(1, "Name1") },
                         { 2, new PollData.PollChoice(2, "Name2") },
-                        { 3, new PollData.PollChoice(3, "Name3") }
+                        { 3, new PollData.PollChoice(3, "Name3") },
                     },
                     WeightedChoices = new PollData.WeightedChoicesList(),
-                }
+                },
             };
 
             var votes = new List<MeetingPollVote>()
             {
-                new MeetingPollVote()
+                new()
                 {
                     IsTiebreaker = true,
                     VotingPower = 2,
                     ParsedVoteContent = new PollVoteData()
                     {
-                        SelectedOptions = new List<int>() { 1, 2 }
-                    }
+                        SelectedOptions = new List<int>() { 1, 2 },
+                    },
                 },
-                new MeetingPollVote()
+                new()
                 {
                     ParsedVoteContent = new PollVoteData()
                     {
-                        SelectedOptions = new List<int>() { 2 }
-                    }
+                        SelectedOptions = new List<int>() { 2 },
+                    },
                 },
-                new MeetingPollVote()
+                new()
                 {
                     ParsedVoteContent = new PollVoteData()
                     {
-                        SelectedOptions = new List<int>() { 3, 2, 1 }
-                    }
+                        SelectedOptions = new List<int>() { 3, 2, 1 },
+                    },
                 },
-                new MeetingPollVote()
+                new()
                 {
                     VotingPower = 2,
                     ParsedVoteContent = new PollVoteData()
                     {
-                        SelectedOptions = new List<int>() { 3 }
-                    }
+                        SelectedOptions = new List<int>() { 3 },
+                    },
                 },
             };
 
@@ -84,6 +84,243 @@ namespace ThriveDevCenter.Server.Tests.Models.Tests
 
             Assert.Equal(1, resultData.Results[2].Item1);
             Assert.Equal(2.3333333333333335, resultData.Results[2].Item2);
+        }
+
+        [Fact]
+        public void Poll_SingleChoicePollCountingWorks()
+        {
+            var poll = new MeetingPoll()
+            {
+                ClosedAt = DateTime.UtcNow,
+                ParsedData = new PollData()
+                {
+                    Choices = new Dictionary<int, PollData.PollChoice>()
+                    {
+                        { 1, new PollData.PollChoice(1, "Name1") },
+                        { 2, new PollData.PollChoice(2, "Name2") },
+                        { 3, new PollData.PollChoice(3, "Name3") },
+                    },
+                    SingleChoiceOption = new PollData.SingleChoice(),
+                },
+            };
+
+            var votes = new List<MeetingPollVote>()
+            {
+                new()
+                {
+                    IsTiebreaker = true,
+                    VotingPower = 2,
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 1 },
+                    },
+                },
+                new()
+                {
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 2 },
+                    },
+                },
+                new()
+                {
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 3 },
+                    },
+                },
+                new()
+                {
+                    VotingPower = 2,
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 3 },
+                    },
+                },
+            };
+
+            poll.CalculateResults(votes);
+
+            Assert.NotNull(poll.PollResults);
+
+            var resultData = poll.ParsedResults;
+
+            Assert.NotNull(resultData);
+            Assert.NotNull(resultData!.Results);
+            Assert.Null(resultData.TiebreakInFavourOf);
+            Assert.Equal(6, resultData.TotalVotes);
+
+            Assert.Equal(3, resultData.Results.Count);
+
+            // Results check
+
+            Assert.Equal(3, resultData.Results[0].Item1);
+            Assert.Equal(3, resultData.Results[0].Item2);
+
+            Assert.Equal(1, resultData.Results[1].Item1);
+            Assert.Equal(2, resultData.Results[1].Item2);
+
+            Assert.Equal(2, resultData.Results[2].Item1);
+            Assert.Equal(1, resultData.Results[2].Item2);
+        }
+
+        /// <summary>
+        ///   Makes sure that if somehow multiple votes for the single poll type are voted, they are not counted
+        /// </summary>
+        [Fact]
+        public void Poll_SinglePollMultipleVotesAreNotCounted()
+        {
+            var poll = new MeetingPoll()
+            {
+                ClosedAt = DateTime.UtcNow,
+                ParsedData = new PollData()
+                {
+                    Choices = new Dictionary<int, PollData.PollChoice>()
+                    {
+                        { 1, new PollData.PollChoice(1, "Name1") },
+                        { 2, new PollData.PollChoice(2, "Name2") },
+                        { 3, new PollData.PollChoice(3, "Name3") },
+                    },
+                    SingleChoiceOption = new PollData.SingleChoice(),
+                },
+            };
+
+            var votes = new List<MeetingPollVote>()
+            {
+                new()
+                {
+                    IsTiebreaker = true,
+                    VotingPower = 2,
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 1, 2 },
+                    },
+                },
+                new()
+                {
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 2 },
+                    },
+                },
+                new()
+                {
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 3 },
+                    },
+                },
+                new()
+                {
+                    VotingPower = 2,
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 3, 1 },
+                    },
+                },
+            };
+
+            poll.CalculateResults(votes);
+
+            Assert.NotNull(poll.PollResults);
+
+            var resultData = poll.ParsedResults;
+
+            Assert.NotNull(resultData);
+            Assert.NotNull(resultData!.Results);
+            Assert.Null(resultData.TiebreakInFavourOf);
+            Assert.Equal(6, resultData.TotalVotes);
+
+            Assert.Equal(3, resultData.Results.Count);
+
+            // Results check
+
+            Assert.Equal(3, resultData.Results[0].Item1);
+            Assert.Equal(3, resultData.Results[0].Item2);
+
+            Assert.Equal(1, resultData.Results[1].Item1);
+            Assert.Equal(2, resultData.Results[1].Item2);
+
+            Assert.Equal(2, resultData.Results[2].Item1);
+            Assert.Equal(1, resultData.Results[2].Item2);
+        }
+
+        [Fact]
+        public void Poll_MultipleChoiceResultsAreRight()
+        {
+            var poll = new MeetingPoll()
+            {
+                ClosedAt = DateTime.UtcNow,
+                ParsedData = new PollData()
+                {
+                    Choices = new Dictionary<int, PollData.PollChoice>()
+                    {
+                        { 1, new PollData.PollChoice(1, "Name1") },
+                        { 2, new PollData.PollChoice(2, "Name2") },
+                        { 3, new PollData.PollChoice(3, "Name3") },
+                    },
+                    MultipleChoiceOption = new PollData.MultipleChoice(),
+                },
+            };
+
+            var votes = new List<MeetingPollVote>()
+            {
+                new()
+                {
+                    IsTiebreaker = true,
+                    VotingPower = 2,
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 1, 2 },
+                    },
+                },
+                new()
+                {
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 2 },
+                    },
+                },
+                new()
+                {
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 3, 2, 1 },
+                    },
+                },
+                new()
+                {
+                    VotingPower = 2,
+                    ParsedVoteContent = new PollVoteData()
+                    {
+                        SelectedOptions = new List<int>() { 3 },
+                    },
+                },
+            };
+
+            poll.CalculateResults(votes);
+
+            Assert.NotNull(poll.PollResults);
+
+            var resultData = poll.ParsedResults;
+
+            Assert.NotNull(resultData);
+            Assert.NotNull(resultData!.Results);
+            Assert.Null(resultData.TiebreakInFavourOf);
+            Assert.Equal(10, resultData.TotalVotes);
+
+            Assert.Equal(3, resultData.Results.Count);
+
+            // Results check
+
+            Assert.Equal(2, resultData.Results[0].Item1);
+            Assert.Equal(4, resultData.Results[0].Item2);
+
+            Assert.Equal(1, resultData.Results[1].Item1);
+            Assert.Equal(3, resultData.Results[1].Item2);
+
+            Assert.Equal(3, resultData.Results[2].Item1);
+            Assert.Equal(3, resultData.Results[2].Item2);
         }
     }
 }
