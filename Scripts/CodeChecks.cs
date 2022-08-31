bf563b1bc0b81136a156fa9a6cded6fb7cb5a6bf
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-08-31 13:41:20 +0300

    Started converting the DevCenter scripts to the new system

---
+++ Scripts/CodeChecks.cs
@@ -0,0 +1,30 @@
+namespace Scripts;
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using ScriptsBase.Checks;
+
+public class CodeChecks : CodeChecksBase<Program.CheckOptions>
+{
+    public CodeChecks(Program.CheckOptions opts) : base(opts)
+    {
+        FilePathsToAlwaysIgnore.Add(new Regex(@"Server/Migrations/"));
+    }
+
+    protected override Dictionary<string, CodeCheck> ValidChecks { get; } = new()
+    {
+        { "files", new FileChecks() },
+        { "compile", new CompileCheck() },
+        { "inspectcode", new InspectCode() },
+
+        // Cleanup doesn't currently work nicely for the DevCenter (due to not working on .razor files)
+        // { "cleanupcode", new CleanupCode() },
+    };
+
+    protected override IEnumerable<string> ForceIgnoredJetbrainsInspections =>
+        new[] { "CSharpErrors", "Html.PathError" };
+
+    protected override IEnumerable<string> ExtraIgnoredJetbrainsInspectWildcards => new[] { "Server/Migrations/*" };
+
+    protected override string MainSolutionFile => "RevolutionaryGamesCommon.sln";
+}
---
+++ Scripts/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using CommandLine;
+using Scripts;
+using ScriptsBase.Models;
+using ScriptsBase.Utilities;
+using SharedBase.Utilities;
+
+public class Program
+{
+    public class CheckOptions : CheckOptionsBase
+    {
+    }
+
+    [Verb("test", HelpText = "Run tests using 'dotnet' command")]
+    public class TestOptions : CheckOptionsBase
+    {
+    }
+
+    [Verb("deploy", HelpText = "Perform site deployment")]
+    public class DeployOptions : CheckOptionsBase
+    {
+    }
+
+    [Verb("ef", HelpText = "Perform EntityFramework (ef) helper operations")]
+    public class EFOptions : CheckOptionsBase
+    {
+    }
+
+    [STAThread]
+    public static int Main(string[] args)
+    {
+        RunFolderChecker.EnsureRightRunningFolder("ThriveDevCenter.sln");
+
+        var result = CommandLineHelpers.CreateParser().ParseArguments<CheckOptions, TestOptions, DeployOptions, EFOptions>(args)
+            .MapResult(
+                (CheckOptions opts) => RunChecks(opts),
+                (TestOptions opts) => RunTests(opts),
+                (DeployOptions opts) => RunDeploy(opts),
+                (EFOptions opts) => RunEF(opts),
+                CommandLineHelpers.PrintCommandLineErrors);
+
+        ConsoleHelpers.CleanConsoleStateForExit();
+
+        return result;
+    }
+
+    private static int RunChecks(CheckOptions opts)
+    {
+        CommandLineHelpers.HandleDefaultOptions(opts);
+
+        ColourConsole.WriteDebugLine("Running in check mode");
+        ColourConsole.WriteDebugLine($"Manually specified checks: {string.Join(' ', opts.Checks)}");
+
+        var checker = new CodeChecks(opts);
+
+        return checker.Run().Result;
+    }
+
+    private static int RunTests(TestOptions opts)
+    {
+        CommandLineHelpers.HandleDefaultOptions(opts);
+
+        ColourConsole.WriteDebugLine("Running dotnet tests");
+
+        var tokenSource = ConsoleHelpers.CreateSimpleConsoleCancellationSource();
+
+        return ProcessRunHelpers.RunProcessAsync(new ProcessStartInfo("dotnet", "test"), tokenSource.Token, false)
+            .Result.ExitCode;
+    }
+    
+    private static int RunDeploy(DeployOptions opts)
+    {
+        CommandLineHelpers.HandleDefaultOptions(opts);
+
+        ColourConsole.WriteInfo("Starting deployment");
+
+        var tokenSource = ConsoleHelpers.CreateSimpleConsoleCancellationSource();
+
+        throw new NotImplementedException();
+    }
+    
+    private static int RunEF(EFOptions opts)
+    {
+        CommandLineHelpers.HandleDefaultOptions(opts);
+
+        ColourConsole.WriteDebugLine("Running EF helper tool");
+
+        var tokenSource = ConsoleHelpers.CreateSimpleConsoleCancellationSource();
+
+        throw new NotImplementedException();
+
+        return ProcessRunHelpers.RunProcessAsync(new ProcessStartInfo("dotnet", "test"), tokenSource.Token, false)
+            .Result.ExitCode;
+    }
+}
