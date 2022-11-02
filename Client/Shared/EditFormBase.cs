096c45e542732e9ef33b396c8742af06bbf1af5e
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-11-02 10:41:13 +0200

    Moved admin feed controls to a subfolder and created a basic base class for edit forms

---
+++ Client/Shared/EditFormBase.cs
@@ -0,0 +1,50 @@
+namespace ThriveDevCenter.Client.Shared;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using ThriveDevCenter.Shared;
+
+/// <summary>
+///   Base class for edit forms
+/// </summary>
+/// <typeparam name="T">The type of object to edit</typeparam>
+public class EditFormBase<T> : ComponentBase
+{
+    [Parameter]
+
+    // ReSharper disable InconsistentNaming
+    public string? @class { get; set; }
+
+#pragma warning disable CS8618
+    [Parameter]
+    [EditorRequired]
+    public EditContext EditContext { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public T EditedData { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public string ControlIdSuffix { get; set; }
+
+#pragma warning restore CS8618
+
+    [Parameter]
+    public string? StatusMessage { get; set; }
+
+    [Parameter]
+    public SiteNoticeType StatusMessageType { get; set; } = SiteNoticeType.Danger;
+
+    [Parameter]
+    public bool Processing { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public EventCallback OnValidSubmit { get; set; }
+
+    [Parameter]
+    public string? OverrideSaveText { get; set; }
+
+    protected string SaveButtonText => string.IsNullOrEmpty(OverrideSaveText) ? "Save Changes" : OverrideSaveText;
+}
