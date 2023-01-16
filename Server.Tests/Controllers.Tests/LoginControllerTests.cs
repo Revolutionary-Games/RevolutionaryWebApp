98f7f89f12dcff2e7aede2e434212f9c73cf2e26
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-01-16 17:20:40 +0200

    Removed unnecessary using

--- Server.Tests/Controllers.Tests/LoginControllerTests.cs
+++ Server.Tests/Controllers.Tests/LoginControllerTests.cs
@@ -2,7 +2,6 @@ namespace ThriveDevCenter.Server.Tests.Controllers.Tests;
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Common.Utilities;
