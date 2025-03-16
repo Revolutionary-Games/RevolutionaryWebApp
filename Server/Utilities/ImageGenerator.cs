2f7b8dc4a5c090e02d0c9a32abd7e09369b53a35
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-03-16 21:36:25 +0200

    Otherwise finished the image generator but no background
    image exists yet for the banners

--- Server/Utilities/ImageGenerator.cs
+++ Server/Utilities/ImageGenerator.cs
@@ -3,7 +3,9 @@ namespace RevolutionaryWebApp.Server.Utilities;
 using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
@@ -12,7 +14,10 @@ using SixLabors.ImageSharp.Formats.Webp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
-public class ImageGenerator
+/// <summary>
+///   Handles dynamic generation of various images
+/// </summary>
+public sealed class ImageGenerator : IDisposable
 {
     private const int LetterAvatarSize = 256;
 
@@ -24,18 +29,46 @@ public class ImageGenerator
 
     private readonly IHttpClientFactory httpClientFactory;
 
+    private readonly SemaphoreSlim fontDownloadSemaphore = new(1, 1);
+
+    private FontFamily? juraFont;
+    private FontFamily? thriveFont;
+
     public ImageGenerator(IHttpClientFactory httpClientFactory)
     {
         this.httpClientFactory = httpClientFactory;
     }
 
+    /// <summary>
+    ///   Helper to generate initials (1 or 2 letters)
+    /// </summary>
+    /// <param name="name">Name / username</param>
+    /// <returns>Initials</returns>
+    public static string GetInitials(string name)
+    {
+        var parts = name.Trim().Split(new[] { ' ', '_', '-' }, StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length == 0)
+            return "?";
+
+        // Single name
+        if (parts.Length == 1)
+        {
+            return parts[0].Substring(0, Math.Min(2, parts[0].Length)).ToUpper();
+        }
+
+        // Use the first letter of the first two words
+        return (parts[0][0] + parts[1][0].ToString()).ToUpper();
+    }
+
     public async Task<byte[]> GenerateProgressUpdateBanner(DateTime date)
     {
-        using var image = new Image<Rgb24>(ProgressUpdateBannerWidth, ProgressUpdateBannerHeight, Color.Black);
+        await GetMissingBannerFontsIfNeeded();
+
+        using var image = new Image<Rgb24>(ProgressUpdateBannerWidth, ProgressUpdateBannerHeight, Color.WhiteSmoke);
 
         // Draw initials in the center
-        // TODO: customize the font (this should be present on most Linuxes)
-        var font = SystemFonts.CreateFont("DejaVu Sans", 96, FontStyle.Bold);
+        var font = juraFont!.Value.CreateFont(96, FontStyle.Regular);
+        var titleFont = thriveFont!.Value.CreateFont(128, FontStyle.Regular);
 
         // TODO: actually implement all the parts and the fancy background image
 
@@ -44,11 +77,22 @@ public class ImageGenerator
             ctx.DrawText(new RichTextOptions(font)
             {
                 TextAlignment = TextAlignment.Center,
-                Font = font,
-                Origin = new PointF(ProgressUpdateBannerWidth / 2.0f, ProgressUpdateBannerHeight / 2.0f),
+                Font = titleFont,
+                Origin = new PointF(ProgressUpdateBannerWidth / 2.0f,
+                    ProgressUpdateBannerHeight / 2.0f - ProgressUpdateBannerHeight / 6.0f),
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center,
-            }, date.ToString("MMMM dd yyyy", CultureInfo.InvariantCulture), Color.White);
+            }, "PROGRESS\nUPDATE", Color.Black);
+
+            ctx.DrawText(new RichTextOptions(font)
+            {
+                TextAlignment = TextAlignment.Center,
+                Font = font,
+                Origin = new PointF(ProgressUpdateBannerWidth / 2.0f,
+                    ProgressUpdateBannerHeight / 2.0f + ProgressUpdateBannerHeight / 4.0f),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+            }, date.ToString("MMMM dd yyyy", CultureInfo.InvariantCulture), Color.Black);
         });
 
         // Save the image after generating
@@ -94,24 +138,60 @@ public class ImageGenerator
         return memoryStream.ToArray();
     }
 
-    /// <summary>
-    ///   Helper to generate initials (1 or 2 letters)
-    /// </summary>
-    /// <param name="name">Name / username</param>
-    /// <returns>Initials</returns>
-    public static string GetInitials(string name)
+    public void Dispose()
     {
-        var parts = name.Trim().Split(new[] { ' ', '_', '-' }, StringSplitOptions.RemoveEmptyEntries);
-        if (parts.Length == 0)
-            return "?";
+        fontDownloadSemaphore.Dispose();
+    }
 
-        // Single name
-        if (parts.Length == 1)
+    private async ValueTask GetMissingBannerFontsIfNeeded()
+    {
+        if (juraFont == null)
         {
-            return parts[0].Substring(0, Math.Min(2, parts[0].Length)).ToUpper();
+            await fontDownloadSemaphore.WaitAsync();
+            try
+            {
+                // In case it just got downloaded
+                if (juraFont == null)
+                {
+                    var collection = await DownloadFont(JuraFontUrl);
+                    juraFont = collection.Families.First();
+                }
+            }
+            finally
+            {
+                fontDownloadSemaphore.Release();
+            }
         }
 
-        // Use the first letter of the first two words
-        return (parts[0][0] + parts[1][0].ToString()).ToUpper();
+        if (thriveFont == null)
+        {
+            await fontDownloadSemaphore.WaitAsync();
+            try
+            {
+                if (thriveFont == null)
+                {
+                    var collection = await DownloadFont(ThriveFontUrl);
+                    thriveFont = collection.Families.First();
+                }
+            }
+            finally
+            {
+                fontDownloadSemaphore.Release();
+            }
+        }
+    }
+
+    private async Task<FontCollection> DownloadFont(string fontUrl)
+    {
+        using var client = httpClientFactory.CreateClient();
+
+        var response = await client.GetAsync(fontUrl);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStreamAsync();
+
+        var collection = new FontCollection();
+        collection.Add(content);
+        return collection;
     }
 }
