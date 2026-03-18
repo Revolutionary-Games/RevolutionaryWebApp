32d236a6c1afdc4daceb1fbd31369cf93c19b4e5
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-03-18 18:36:09 +0200

    Quota DB migration

---
+++ Server/Migrations/20260318163546_AddUserUploadQuota.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RevolutionaryWebApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserUploadQuota : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "upload_quota_used",
+                table: "users",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "upload_quota_used",
+                table: "users");
+        }
+    }
+}
