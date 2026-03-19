28b9f3f5f5535f1429069b0bfe09f434f4451dde
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-03-19 10:47:09 +0200

    Delete old page versions in a migration
    as the data is not compatible. Everything seems to work but new page edits now reset the version back.

---
+++ Server/Migrations/20260318164744_DeleteOldPageVersions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RevolutionaryWebApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteOldPageVersions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // We intentionally delete *everything* as the data is simply not compatible!
+            migrationBuilder.Sql("-- noinspection SqlWithoutWhereForFile\n"+
+            "DELETE FROM page_versions;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
