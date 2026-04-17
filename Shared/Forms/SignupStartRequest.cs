23829b69dfe31927b9fa407390cf9508835d9f38
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-04-17 11:09:11 +0300

    Convert the registration form to new format

--- Shared/Forms/SignupStartRequest.cs
+++ Shared/Forms/SignupStartRequest.cs
@@ -1,6 +1,8 @@
 ﻿namespace RevolutionaryWebApp.Shared.Forms;
 
 using System.ComponentModel.DataAnnotations;
+using DevCenterCommunication;
+using SharedBase.ModelVerifiers;
 using SharedBase.Utilities;
 
 public class SignupStartRequest
@@ -11,13 +13,14 @@ public class SignupStartRequest
     public string Email { get; set; } = string.Empty;
 
     [Required]
-    [MaxLength(500)]
+    [MaxLength(CommunicationConstants.MAXIMUM_TOKEN_LENGTH)]
     public string CSRF { get; set; } = string.Empty;
 }
 
 public class PendingSignupInfoDTO
 {
     [MaxLength(GlobalConstants.MaxEmailLength)]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
 }
 
@@ -28,15 +31,17 @@ public class SignupCompleteRequest
     public string Token { get; set; } = string.Empty;
 
     [Required]
-    [MinLength(3)]
-    [MaxLength(64)]
+    [StringLength(CommunicationConstants.MAX_USERNAME_LENGTH,
+        MinimumLength = CommunicationConstants.MIN_USERNAME_LENGTH)]
+    [NoTrailingOrPrecedingSpace]
     public string UserName { get; set; } = string.Empty;
 
     [MaxLength(128)]
+    [NoTrailingOrPrecedingSpace]
     public string? DisplayName { get; set; }
 
     [Required]
-    [MaxLength(500)]
+    [MaxLength(CommunicationConstants.MAXIMUM_TOKEN_LENGTH)]
     public string CSRF { get; set; } = string.Empty;
 
     [MaxLength(500)]
