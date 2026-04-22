4a4095f229aaa5f09693de8b9cd0069ec35e3218
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-04-22 12:45:51 +0300

    Extended podman image info JSON parsing

--- CIExecutor/PodmanImageInfo.cs
+++ CIExecutor/PodmanImageInfo.cs
@@ -1,7 +1,7 @@
 namespace CIExecutor;
 
-using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 /// <summary>
 ///   Parsed image info from podman JSON format
@@ -15,9 +15,24 @@ public class PodmanImageInfo
 
     public int Containers { get; set; }
 
+    /// <summary>
+    ///   Numeric creation time from podman JSON. In many versions this is Unix epoch seconds.
+    /// </summary>
     public long Created { get; set; }
 
-    public DateTime CreatedAt { get; set; }
+    /// <summary>
+    ///   Raw string creation time (podman uses formats like
+    ///   "YYYY-MM-DD HH:MM:SS +0000 UTC" or ISO8601 "YYYY-MM-DDTHH:MM:SSZ").
+    ///   Keep as string to parse robustly.
+    /// </summary>
+    [JsonPropertyName("CreatedAt")]
+    public string? CreatedAtRaw { get; set; }
+
+    /// <summary>
+    ///   Some podman versions expose CreatedTime as an alias for CreatedAt.
+    /// </summary>
+    [JsonPropertyName("CreatedTime")]
+    public string? CreatedTimeRaw { get; set; }
 
     public bool Dangling { get; set; }
 
